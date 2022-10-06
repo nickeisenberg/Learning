0ab17addc48c5086e87b8a545701c80a291903ff
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-10-06 09:50:08 -0700

    polynomial example

--- Misc/poly_approx.py
+++ Misc/poly_approx.py
@@ -29,7 +29,8 @@ def poly_approx(fun, dom_l=0, dom_h=1, part_size=100, order=1, noise=False):
 
 #########################Some Examples#########################
 
-def sin_approx(dom_l=0, dom_h=3*np.pi, order=5, part_size=5000, noise=False):
+'''
+def approx_sin(dom_l=0, dom_h=3*np.pi, order=5, part_size=5000, noise=False):
     coef = poly_approx(np.sin, dom_l=dom_l, dom_h=dom_h, order=order)
     domain = np.linspace(dom_l, dom_h, part_size)
     values = []
@@ -42,9 +43,34 @@ def sin_approx(dom_l=0, dom_h=3*np.pi, order=5, part_size=5000, noise=False):
     return values
 
 domain = np.linspace(0, 3*np.pi, 5000)
-values_sin = sin_approx()
+values_sin = approx_sin()
 
 # plt.plot(domain, np.sin(domain) + np.sqrt(2*np.pi/5000)*np.random.randn(5000))
 plt.plot(domain, np.sin(domain))
 plt.plot(domain, values_sin)
 plt.show()
+'''
+
+'''
+def approx_poly(dom_l=-4, dom_h=4, order=5, part_size=5000, noise=False):
+    fun = lambda x : x ** 4 - 3 * x ** 2 + x + 1
+    coef = poly_approx(fun, dom_l=dom_l, dom_h=dom_h, order=order, noise=noise)
+    domain = np.linspace(dom_l, dom_h, part_size)
+    values = []
+    for t in domain:
+        summands = []
+        for a in range(len(coef)):
+            summands.append(coef[a] * t ** a)
+        summands = np.array(summands)
+        values.append(np.sum(summands))
+    return values
+
+domain = np.linspace(-4, 4, 5000)
+values = approx_poly()
+
+fun_ = lambda x : x ** 4 - 3 * x ** 2 + x + 1
+plt.plot(domain, fun_(domain), label='$x^4 - 3 x^2 + x + 1$')
+plt.plot(domain, values, ':', label='Order 4 approximation')
+plt.legend(loc='upper center')
+plt.show()
+'''
