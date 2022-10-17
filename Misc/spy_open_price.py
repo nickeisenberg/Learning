ff24a42b69ce7aaadd9b0c26a3d526b4ea0323fa
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-10-17 13:50:31 -0700

    some fft time series stuff for gme

--- Misc/spy_open_price.py
+++ Misc/spy_open_price.py
@@ -1,22 +1,78 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import medfilt, wiener
 
-spy_info = yf.Ticker('SPY')
+stock_info = yf.Ticker('GME')
 
-price_history = spy_info.history(period='5y',
-                                 interval='1d',
+price_history = stock_info.history(period='2y',
+                                 interval='1h',
                                  actions=False)
 
+open_price = price_history['Open'].values
 
-spy_open = price_history['Open'].values[:500]
-time = np.linspace(0, 5, len(spy_open))
+# Min/Max scaler
+max_open = np.max(open_price)
+min_open = np.min(open_price)
+open_price -= min_open
+open_price /= max_open - min_open
 
-freq = np.fft.rfftfreq(len(spy_open))
-fft = abs(np.fft.rfft(spy_open))
+# Wiener filtered data
+open_price_wiener = wiener(open_price, 9)
 
-# plt.plot(time, spy_open)
-plt.plot(freq[1:], fft[1:])
+# median filtered data
+open_price_med = medfilt(open_price, 9)
+
+# Plotting a spectrogram
+time_window = 32
+time_step = 32
+time_unit = 1
+
+spectrogram = []
+spec_times = []
+freqs = np.fft.rfftfreq(time_window, d=time_unit)
+
+for time_start in np.arange(0, len(open_price), time_step):
+
+    data_time_ref = time_start + time_step / 2
+    data = open_price[time_start : time_start + time_step]
+
+    if len(data) < time_window:
+        continue
+
+    spec_times.append(data_time_ref)
+    spectrogram.append(np.abs(np.fft.rfft(data)))
+
+spectrogram = np.array(spectrogram).T
+spec_times = np.array(spec_times)
+
+plt.subplot(121)
+plt.plot(freqs, spectrogram[:,2])
+plt.subplot(122)
+plt.plot(freqs, spectrogram[:,9])
 plt.show()
 
+'''
+fig, ax = plt.subplots()
+x2d, y2d = np.meshgrid(spec_times, freqs)
+pc = ax.pcolormesh(spec_times, freqs, spectrogram, shading='auto')
+fig.colorbar(pc)
+plt.show()
+'''
 
+'''
+time = np.linspace(0, 2, len(open_price))
+
+print(len(open_price))
+freq = np.fft.rfftfreq(len(open_price))
+fft = abs(np.fft.rfft(open_price))
+
+plt.subplot(121)
+plt.plot(time, open_price)
+
+plt.subplot(122)
+plt.plot(freq[1:], fft[1:])
+
+plt.show()
+
+'''
