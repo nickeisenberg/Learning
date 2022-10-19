a003326908d4cc06c440bc48395e0bb5355a8655
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-10-19 11:52:04 -0700

    code folding

--- Misc/compiler.py
+++ Misc/compiler.py
@@ -2,5 +2,9 @@ import pandas as pd
 import numpy as np
 from matplotlib.pyplot import plot, show, subplot
 
+#-fold-1-------------------------------------------
 x = [1, 2, 3, 4, 5, 6]
+
+#-fold-2-------------------------------------------
+y = [1, 1, 1]
 
