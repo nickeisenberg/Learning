d58c4ef95c39dcde6b78b837772a1aa5d0061427
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-10-31 07:50:46 -0700

    changed folder name

---
+++ MinimalWorkingExamples/complier2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# print('')
+# print([1, 2, 3, 4])
+# print([5, 6, 7, 8])
+
+time = np.linspace(0, 1, 100)
+data = np.sin(2 * np.pi * time)
+
+plt.plot(time, data)
+plt.show()
+
+print([1,1,1])
+print([1,1,1])
+print([1,1,1])
--- Misc/command.vim
+++
@@ -1,1 +0,0 @@
-nnoremap <leader><leader> :w<cr>:!python3 % & <cr>
