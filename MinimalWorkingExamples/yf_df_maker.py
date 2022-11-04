d17c89e160a8e7d45d1358186c48e8256c578fec
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-11-03 17:06:46 -0700

    pushing changes

--- MinimalWorkingExamples/yf_df_maker.py
+++ MinimalWorkingExamples/yf_df_maker.py
@@ -18,7 +18,5 @@ gme['Percent_Change'] = p_change
 
 print(gme.head(15))
 
-gme_2y = pd.DataFrame(gme_df_2y['Open'], columns=['Open'])
 
-# gme_2y.to_csv('/Users/nickeisenberg/GitRepos/Python_Misc/MinimalWorkingExamples/DataSets/gme_11_3_22.csv')
 
