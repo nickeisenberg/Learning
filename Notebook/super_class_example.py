a8f218189b7983a2773e11043127de0fd96742d1
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-12-09 18:18:20 -0800

    readme

---
+++ Notebook/super_class_example.py
@@ -0,0 +1,35 @@
+class parent:
+
+    def __init__(self, text, a, b):
+        self.text = text
+        self.a = a
+        self.b = b
+
+    def print_text(self):
+        print(self.text)
+
+    def add_nums(self):
+        print(self.a + self.b)
+
+class child(parent):
+
+    def __init__(self, text, c, d):
+        super().__init__(text, 2, 3)
+        self.c = c
+        self.d = d
+
+    def subtract_nums(self):
+        print(self.c - self.d)
+
+child_instance = child(text='here is the text',
+                       c=1,
+                       d=2)
+
+print(child_instance.a)
+print(child_instance.b)
+print(child_instance.c)
+print(child_instance.d)
+
+child_instance.print_text()
+child_instance.add_nums()
+child_instance.subtract_nums()
