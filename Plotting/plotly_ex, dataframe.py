d6d1e794b1a43b4446342db3e55fdb81a01c15ba
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2023-02-02 09:35:49 -0800

    plotting from a data frame

---
+++ Plotting/plotly_ex, dataframe.py
@@ -0,0 +1,18 @@
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+bm = np.random.normal(size=(1000, 10))
+bm *= np.sqrt(1 / 1000)
+bm = np.cumsum(bm, axis=0)
+
+time = np.linspace(0, 1, 1000)
+
+bm_df = pd.DataFrame(
+        data=bm,
+        index=time)
+
+fig = px.line(bm_df, x=bm_df.index, y=bm_df.columns)
+fig.show()
+
+print(bm_df.columns)
