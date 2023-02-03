24a34cb70e66540f7987f34ea5f2fd846540ee8d
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2023-02-02 17:37:49 -0800

    plotly color by number

--- Plotting/mpl_color_by_number.py
+++ Plotting/mpl_color_by_number.py
@@ -6,11 +6,12 @@ line = lambda x, b: x + b
 
 lines = np.array(
         [line(np.linspace(0, 1, 100), b)
-         for b in range(10)]
+         for b in np.linspace(0, 1, 100)]
         )
 
 cmap = plt.get_cmap('viridis', lines.shape[0])
 norm = mplc.Normalize(vmin=0, vmax=lines.shape[0] - 1)
+
 for i, l in enumerate(lines):
     plt.plot(l, color=cmap(norm(i)))
 plt.title('color by number')
