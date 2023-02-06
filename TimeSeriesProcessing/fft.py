f8d75fdf847a2caeb8dc0759e207ddcb1f6d7ca8
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2023-02-06 15:52:44 -0800

    updataed the spectrogram

--- TimeSeriesProcessing/fft.py
+++ TimeSeriesProcessing/fft.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -34,21 +35,35 @@ def plot_specrogram_ptly(freqs, ffts, show=False):
         return fig
 
 def plot_spectrogram_mpl(freqs, ffts, show=False):
-    t = np.array([np.ones(ffts.shape[1]) * i for i in range(ffts.shape[0])])
-    t = t.reshape(-1)
-    freqs = np.array([freqs for i in range(ffts.shape[0])]).reshape(-1)
-    ffts = ffts.reshape(-1)
-    plot = plt.scatter(t, freqs, c=ffts)
+    t = np.linspace(0, ffts.shape[0] - 1, ffts.shape[0])
+    xx, yy = np.meshgrid(t, freqs)
+    fig, ax = plt.subplots()
+    ax.pcolormesh(xx, yy, ffts.T,
+                  shading='auto',
+                  norm=colors.Normalize(ffts.min(), ffts.max()),
+                  cmap=plt.cm.viridis)
     if show:
         return plt.show()
     else:
-        return plot
+        return fig, ax
+
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+
+xx, yy = np.meshgrid(x, y)
+
 #--------------------------------------------------
 
 # test the functions
 time =np.linspace(0, 10000, 10001)
 data = np.sin(1 / 2 * np.pi * time)
 
+freqs, ffts = spectrogram(data)
+
+fig, ax = plot_spectrogram_mpl(freqs, ffts)
+plt.show()
+
+#  view the data
 fft = np.abs(np.fft.rfft(data))
 freqs = np.fft.rfftfreq(time.shape[0])
 
@@ -72,12 +87,3 @@ _ = fig.update_layout(
             'text': 'text',
             })
 fig.show()
-
-freqs, ffts = spectrogram(data)
-
-fig = plot_specrogram_ptly(freqs, ffts)
-fig.show()
-
-plot = plot_spectrogram_mpl(freqs, ffts)
-plt.show()
-
