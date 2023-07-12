181a38a68146d70369a69d2079a02d05a5c70b60
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-12 11:41:29 -0700

    seeing if clusters of shared words form.

--- Reddit_Comment_Classification/tech_vs_sports/eda.py
+++ Reddit_Comment_Classification/tech_vs_sports/eda.py
@@ -1,7 +1,6 @@
 import reddit_funcs as rfunc
 import numpy as np
-import keras
-from keras import layers, Model, Input, Sequential
+import matplotlib.pyplot as plt
 
 path = '/Users/nickeisenberg/GitRepos/'
 path += 'DataSets_local/subreddit_texts/tech_vs_sport/'
@@ -19,12 +18,38 @@ sport_proc.get_words(
 )
 sport_proc.get_vocab(no_words=10000)
 
-tech_proc.vocab
-sport_proc.vocab
+# tech and sport words
+tech_words = np.array([*tech_proc.vocab.keys()])
+sport_words = np.array([*sport_proc.vocab.keys()])
 
-count = 0
-for k in tech_proc.vocab:
-    if k in sport_proc.vocab.keys():
-        count += 1
-count
+# shared, total and unique
+shared = np.intersect1d(tech_words, sport_words)
+total = np.unique(np.hstack((tech_words, sport_words)))
 
+total_key = {word: i for i, word in enumerate(total)}
+shared_key = {word: total_key[word] for word in shared}
+
+shared_count_tech = {}
+tt = []
+for word, i in shared_key.items():
+    if word == '[UNK]':
+        continue
+    shared_count_tech[i] = tech_proc.word_counts[word]
+    tt.append([i, tech_proc.word_counts[word]])
+
+shared_count_sport = {}
+ss = []
+for word, i in shared_key.items():
+    if word == '[UNK]':
+        continue
+    shared_count_sport[i] = sport_proc.word_counts[word]
+    ss.append([i, sport_proc.word_counts[word]])
+
+tt = np.array(tt)
+ss = np.array(ss)
+
+tt[:4]
+ss[:4]
+
+plt.scatter(tt[:,1], ss[:,1])
+plt.show()
