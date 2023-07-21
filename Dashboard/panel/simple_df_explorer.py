043cf0be77e76b080f7eb6610e1a75fb5169fd5d
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-21 10:40:39 -0700

    working on the xlim control

--- Dashboard/panel/simple_df_explorer.py
+++ Dashboard/panel/simple_df_explorer.py
@@ -34,21 +34,25 @@ class df_exp(Viewer):
 
         super().__init__(**params)
 
-        # First tab controls
+        #-Set the controls for the tabs--------------------
+        # Tab 1
         self._main_controls = pn.Param(
             self.param, parameters=['axis', 'sensor', 'shot'],
             sizing_mode='stretch_width', max_width=300, show_name=False,
         )
-
+        
+        # Tab 2
         self._axes_controls = pn.Param(
             self.param, parameters=['xlim', 'ylim'],
             sizing_mode='stretch_width', max_width=300, show_name=False,
         )
-
+        
+        # Tab 3
         self._nt_controls = pn.Param(
             self.param, parameters=['text'],
             sizing_mode='stretch_width', max_width=300, show_name=False,
         )
+        #--------------------------------------------------
 
         self._tabs = pn.Tabs(
             tabs_location='left', width=400
@@ -56,6 +60,7 @@ class df_exp(Viewer):
         self._alert = pn.pane.Alert(
             alert_type='danger', visible=False, sizing_mode='stretch_width'
         )
+
         self._layout = pn.Column(
             pn.Row(
                 self._tabs,
@@ -66,26 +71,12 @@ class df_exp(Viewer):
             sizing_mode='stretch_both'
         )
         
-        self._toggle_controls()
+        self._toggle_main()
         self._retrieve_parquet()
         self._plot()
 
-    @param.depends('axis', 'sensor', watch=True)
-    def _retrieve_parquet(self):
-        self._parquet = pq.read_table(
-            f'./data/{self.axis}/{self.sensor}.parquet'
-        )
-        self._parq_df = self._parquet.to_pandas()
-
-    @param.depends('axis', 'sensor', watch=True)
-    def _update_shots(self):
-        shots = SENSOR_SHOTS[self.axis][self.sensor]
-        self.param['shot'].objects = shots
-        if self.shot not in shots:
-            self.shot = shots[0]
-
     @param.depends('axis', 'sensor', 'shot', watch=True)
-    def _toggle_controls(self):
+    def _toggle_main(self):
         parameters = ['axis', 'sensor', 'shot']
         self._main_controls.parameters = parameters
         # Control other tabs
@@ -96,10 +87,32 @@ class df_exp(Viewer):
         ]
         self._tabs[:] = tabs
 
+    @param.depends('axis', 'sensor', watch=True)
+    def _update_main(self):
+        shots = SENSOR_SHOTS[self.axis][self.sensor]
+        self.param['shot'].objects = shots
+        if self.shot not in shots:
+            self.shot = shots[0]
+
+    @param.depends('axis', 'sensor', watch=True)
+    def _retrieve_parquet(self):
+        self._parquet = pq.read_table(
+            f'./data/{self.axis}/{self.sensor}.parquet'
+        )
+        self._parq_df = self._parquet.to_pandas()
+
     @param.depends('axis', 'sensor', 'shot', watch=True)
+    def _update_axes(self):
+        domain = self._parq_df[self.shot].index.values
+        self.param['xlim'].bounds = [domain[0], domain[-1]]
+
+    @param.depends('axis', 'sensor', 'shot', 'xlim', watch=True)
     def _plot(self):
         try:
-            self._hvplot = self._parq_df.hvplot.line(y=str(self.shot))
+            iloc0 = self.xlim[0]
+            iloc1 = self.xlim[-1]
+            df = self._parq_df.iloc[iloc0: iloc1]
+            self._hvplot = df.hvplot.line(y=str(self.shot))
             self._hvpane = pn.pane.HoloViews(
                 self._hvplot,
                 sizing_mode='stretch_width',
@@ -111,7 +124,7 @@ class df_exp(Viewer):
             print(self.shot)
             print(self.shot)
 
-
 inst = df_exp()
+
 
 inst.show()
