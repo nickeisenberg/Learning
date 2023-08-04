40d1547f9d5542cbf4feb600aaf31c70f76fed1d
Nick Eisenberg <nickeisenberg@gmail.com>
2023-08-04 07:33:43 -0700

    simple back prop with pytorch and tensorflow

--- Pytorch/back_prop/back_prop.py
+++ Pytorch/back_prop/back_prop.py
@@ -1,21 +1,61 @@
 import torch
 
-w = torch.tensor([0.8], requires_grad=True)
-b = torch.tensor([0.2], requires_grad=True)
+w = torch.tensor([[0.5]], requires_grad=True)
+b = torch.tensor([0.5], requires_grad=True)
 
 x = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
 y_true = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
 
+def forward(w, b):
+    return torch.matmul(x, w) + b
+
+def loss(w, b):
+    return torch.mean((forward(w, b) - y_true) ** 2)
+
+forward(w, b)
+loss(w, b)
+
 learning_rate = .001
 
-y_pred = torch.matmul(x, w) + b
-
 for i in range(5000):
-    y_pred = torch.matmul(x, w) + b
-    loss = torch.mean((y_pred - y_true) ** 2)
-    loss.backward()
+    _loss = loss(w, b)
+    _loss.backward()
     with torch.no_grad():
         w -= w.grad * learning_rate
         b -= b.grad * learning_rate
         _ = w.grad.zero_()
         _ = b.grad.zero_()
+
+
+forward(w, b)
+loss(w, b)
+
+#--------------------------------------------------
+import tensorflow as tf
+
+w = tf.Variable(initial_value=[[.5]])
+b = tf.Variable(initial_value=[.5])
+
+x = tf.Variable([[1], [2], [3], [4]], dtype=tf.float32)
+y_true = tf.Variable([[1], [2], [3], [4]], dtype=tf.float32)
+
+
+def forward(w, b):
+    return tf.matmul(x, w) + b
+
+def loss(w, b):
+    return tf.reduce_mean((forward(w, b) - y_true) ** 2)
+
+forward(w, b)
+loss(w, b)
+
+learning_rate = .001
+
+for i in range(5000):
+    with tf.GradientTape() as tape:
+        _loss = loss(w, b)
+    grads = tape.gradient(_loss, [w, b])
+    _ = w.assign_sub(grads[0] * learning_rate)
+    _ = b.assign_sub(grads[1] * learning_rate)
+
+forward(w, b)
