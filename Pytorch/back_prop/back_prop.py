d285dcb3a75a824ed182e7055c6955c16615699c
Nick Eisenberg <nickeisenberg@gmail.com>
2023-08-04 08:11:24 -0700

    speed test of pytorch vs tensorflow

--- Pytorch/back_prop/back_prop.py
+++ Pytorch/back_prop/back_prop.py
@@ -1,3 +1,9 @@
+'''
+Speed comparision of pytorch and tesnorflow with a simply back property 
+Pytorch is much faster
+'''
+
+
 import torch
 
 w = torch.tensor([[0.5]], requires_grad=True)
@@ -59,3 +65,60 @@ for i in range(5000):
     _ = b.assign_sub(grads[1] * learning_rate)
 
 forward(w, b)
+loss(w, b)
+
+#--------------------------------------------------
+
+class Linear_1D:
+
+    def __init__(self, input_dim, output_dim):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.w = torch.tensor(
+            data=torch.randn(input_dim, output_dim), requires_grad=True
+        )
+        self.b = torch.tensor(
+            data=torch.randn(output_dim), requires_grad=True
+        )
+        self.learning_rate = .001
+
+    def forward(self, data):
+        return torch.matmul(data, self.w) + self.b
+
+    def loss(self, data, y_true):
+        return torch.mean((self.forward(data) - y_true)**2)
+
+    def train(self, data, y_true):
+        _loss = self.loss(data, y_true)
+        _loss.backward()
+        with torch.no_grad():
+            self.w -= self.w.grad * self.learning_rate
+            self.b -= self.b.grad * self.learning_rate
+            _ = self.w.grad.zero_()
+            _ = self.b.grad.zero_()
+
+x = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+y_true = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+
+model = Linear_1D(4, 4)
+
+for i in range(5000):
+    _ = model.train(x, y_true)
+
+model.forward(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
